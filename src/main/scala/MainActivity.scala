72e371070d959eb2f7073f6994dafae300f96ed9
Jack O'Connor <oconnor663@gmail.com>
2013-08-29 22:17:17 -0700

    synchronized maps

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -5,13 +5,15 @@ import _root_.android.os.Bundle
 import android.widget.{TextView, EditText}
 import android.text.{Editable, TextWatcher}
 import android.util.Log
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable._
 
 import scala.actors.Actor
 
 object Logic {
-  val allPalCache = scala.collection.mutable.HashMap.empty[Int, ArrayBuffer[Int]]
-  val pairsCache = scala.collection.mutable.HashMap.empty[Int, ArrayBuffer[Int]]
+  val allPalCache = new HashMap[Int, ArrayBuffer[Int]]
+    with SynchronizedMap[Int, ArrayBuffer[Int]]
+  val pairsCache = new HashMap[Int, ArrayBuffer[Int]]
+    with SynchronizedMap[Int, ArrayBuffer[Int]]
 
   def priceToCents(price: String): Int = {
     val parts = price.split("\\.")
